eb0f629fc68b2a23c4a84308986a2fc0e2e4d53d
Xumin Yu <1090414006@qq.com>
2022-11-21 14:17:33 +0800

    Update dgcnn_group.py

--- models/dgcnn_group.py
+++ models/dgcnn_group.py
@@ -1,8 +1,43 @@
 import torch
 from torch import nn
 from pointnet2_ops import pointnet2_utils
-from knn_cuda import KNN
-knn = KNN(k=16, transpose_mode=False)
+# from knn_cuda import KNN
+# knn = KNN(k=16, transpose_mode=False)
+
+
+def knn_point(nsample, xyz, new_xyz):
+    """
+    Input:
+        nsample: max sample number in local region
+        xyz: all points, [B, N, C]
+        new_xyz: query points, [B, S, C]
+    Return:
+        group_idx: grouped points index, [B, S, nsample]
+    """
+    sqrdists = square_distance(new_xyz, xyz)
+    _, group_idx = torch.topk(sqrdists, nsample, dim = -1, largest=False, sorted=False)
+    return group_idx
+
+def square_distance(src, dst):
+    """
+    Calculate Euclid distance between each two points.
+    src^T * dst = xn * xm + yn * ym + zn * zm
+    sum(src^2, dim=-1) = xn*xn + yn*yn + zn*zn;
+    sum(dst^2, dim=-1) = xm*xm + ym*ym + zm*zm;
+    dist = (xn - xm)^2 + (yn - ym)^2 + (zn - zm)^2
+         = sum(src**2,dim=-1)+sum(dst**2,dim=-1)-2*src^T*dst
+    Input:
+        src: source points, [B, N, C]
+        dst: target points, [B, M, C]
+    Output:
+        dist: per-point square distance, [B, N, M]
+    """
+    B, N, _ = src.shape
+    _, M, _ = dst.shape
+    dist = -2 * torch.matmul(src, dst.permute(0, 2, 1))
+    dist += torch.sum(src ** 2, -1).view(B, N, 1)
+    dist += torch.sum(dst ** 2, -1).view(B, 1, M)
+    return dist  
 
 
 class DGCNN_Grouper(nn.Module):
@@ -63,7 +98,9 @@ class DGCNN_Grouper(nn.Module):
         num_points_q = x_q.size(2)
 
         with torch.no_grad():
-            _, idx = knn(coor_k, coor_q)  # bs k np
+#             _, idx = knn(coor_k, coor_q)  # bs k np
+            idx = knn_point(k, coor_k.transpose(-1, -2).contiguous(), coor_q.transpose(-1, -2).contiguous()) # B G M
+            idx = idx.transpose(-1, -2).contiguous()
             assert idx.shape[1] == k
             idx_base = torch.arange(0, batch_size, device=x_q.device).view(-1, 1, 1) * num_points_k
             idx = idx + idx_base
