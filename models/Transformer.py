031d62dfd74a1601a66940aff57bba2c2754878e
Xumin Yu <1090414006@qq.com>
2022-11-22 16:24:56 +0800

    Update Transformer.py

--- models/Transformer.py
+++ models/Transformer.py
@@ -6,8 +6,42 @@ from timm.models.layers import DropPath,trunc_normal_
 from .dgcnn_group import DGCNN_Grouper
 from utils.logger import *
 import numpy as np
-from knn_cuda import KNN
-knn = KNN(k=8, transpose_mode=False)
+# from knn_cuda import KNN
+# knn = KNN(k=8, transpose_mode=False)
+
+def knn_point(nsample, xyz, new_xyz):
+    """
+    Input:
+        nsample: max sample number in local region
+        xyz: all points, [B, N, C]
+        new_xyz: query points, [B, S, C]
+    Return:
+        group_idx: grouped points index, [B, S, nsample]
+    """
+    sqrdists = square_distance(new_xyz, xyz)
+    _, group_idx = torch.topk(sqrdists, nsample, dim = -1, largest=False, sorted=False)
+    return group_idx
+
+def square_distance(src, dst):
+    """
+    Calculate Euclid distance between each two points.
+    src^T * dst = xn * xm + yn * ym + zn * zm;
+    sum(src^2, dim=-1) = xn*xn + yn*yn + zn*zn;
+    sum(dst^2, dim=-1) = xm*xm + ym*ym + zm*zm;
+    dist = (xn - xm)^2 + (yn - ym)^2 + (zn - zm)^2
+         = sum(src**2,dim=-1)+sum(dst**2,dim=-1)-2*src^T*dst
+    Input:
+        src: source points, [B, N, C]
+        dst: target points, [B, M, C]
+    Output:
+        dist: per-point square distance, [B, N, M]
+    """
+    B, N, _ = src.shape
+    _, M, _ = dst.shape
+    dist = -2 * torch.matmul(src, dst.permute(0, 2, 1))
+    dist += torch.sum(src ** 2, -1).view(B, N, 1)
+    dist += torch.sum(dst ** 2, -1).view(B, 1, M)
+    return dist   
 
 def get_knn_index(coor_q, coor_k=None):
     coor_k = coor_k if coor_k is not None else coor_q
@@ -16,7 +50,9 @@ def get_knn_index(coor_q, coor_k=None):
     num_points_k = coor_k.size(2)
 
     with torch.no_grad():
-        _, idx = knn(coor_k, coor_q)  # bs k np
+#         _, idx = knn(coor_k, coor_q)  # bs k np
+        idx = knn_point(8, coor_k.transpose(-1, -2).contiguous(), coor_q.transpose(-1, -2).contiguous()) # B G M
+        idx = idx.transpose(-1, -2).contiguous()
         idx_base = torch.arange(0, batch_size, device=coor_q.device).view(-1, 1, 1) * num_points_k
         idx = idx + idx_base
         idx = idx.view(-1)
